518e8c09b55d8cc3bf60b0bab08835f67ce08671
halu5071 <horiiortho5@gmail.com>
2019-04-08 15:55:51 +0900

    Update document in English

--- generate_qpsk_signal.m
+++ generate_qpsk_signal.m
@@ -1,6 +1,6 @@
-% ランダムなQPSK用のシンボルを生成します
-% @param size: 生成されるQPSK信号ベクター長
-% @return s: 指定された信号長を持つQPSK信号ベクター
+% generate random QPSK signals
+% @param size: the length of QPSK signal you want.
+% @return s: the vector of QPSK signal.
 function s = generate_qpsk_signal(size)
 M = 4;
 data = randi([0 M-1], size, 1);
